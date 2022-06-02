5db76a459324ff3e25ea59fb8640b565d3c22e27
kkan0615 <kkan0615@gmail.com>
2022-06-02 12:28:46 -0400

    :rocket: version 0.0.5

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
 import { defineConfig } from 'vite'
 import * as path from 'path'
 import vue from '@vitejs/plugin-vue'
-// import typescript from '@rollup/plugin-typescript'
 import typescript2 from 'rollup-plugin-typescript2'
 import { fileURLToPath } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    // typescript({
-    //   tsconfig: './tsconfig.json',
-    //   compilerOptions: {
-    //     declaration: true,
-    //   },
-    // }),
     vue(),
     typescript2({
       check: false,
@@ -50,25 +43,6 @@ export default defineConfig({
       }
     }
   },
-  // build: {
-  //   lib: {
-  //     entry: path.resolve(__dirname, 'src/index.ts'),
-  //     name: 'VueVideoPlayer',
-  //     fileName: (format) => `vue-video-player.${format}.js`
-  //   },
-  //   rollupOptions: {
-  //     // make sure to externalize deps that shouldn't be bundled
-  //     // into your library
-  //     external: ['vue'],
-  //     output: {
-  //       // Provide global variables to use in the UMD build
-  //       // for externalized deps
-  //       globals: {
-  //         vue: 'Vue'
-  //       }
-  //     }
-  //   }
-  // },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
